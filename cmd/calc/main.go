4580b0cfcb5fc3c662bf42da33e64de9d683c13a
Wendell Sun <iwendellsun@gmail.com>
2021-03-26 03:05:27 +0800

    Add calc cmd

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/xwjdsh/calc"
 )
 
+// example: go run main.go -m '{"a":1}' 'sum($a,2,3)+2*3'
+
 func main() {
-	fmt.Println(calc.Eval("sum(1+$b*$a,$c)", map[string]interface{}{"a": 1, "b": 2, "c": 3}))
+	var mapJSON string
+	flag.StringVar(&mapJSON, "m", "", "variable map, JSON format")
+	flag.Parse()
+
+	values := flag.Args()
+	if len(values) == 0 {
+		fmt.Println("Usage: require argument")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	m := map[string]interface{}{}
+	if mapJSON != "" {
+		if err := json.Unmarshal([]byte(mapJSON), &m); err != nil {
+			fmt.Println(`Usage: -m flag require a JSON string, example: {"a": 1, "b": 2}`)
+			flag.PrintDefaults()
+			os.Exit(1)
+		}
+	}
+
+	result, err := calc.Eval(strings.Join(values, ""), m)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println(result)
 }
